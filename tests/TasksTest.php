4770cc24d99d4d5e808cc551ad9d0ff775b0261c
Ali <ma.essa.pk@gmail.com>
2018-01-24 21:44:30 +0000

    Add the test to check if the user can view all tasks in JSON API format

--- tests/TasksTest.php
+++ tests/TasksTest.php
@@ -15,15 +15,57 @@ class TasksTest extends TestCase
         $task = factory(Task::class)->create();
 
         $this->json('GET', "/api/tasks/{$task->id}")
-            ->seeJson([
+            ->seeJsonEquals([
                         'data' => [
                             'type' => 'tasks',
                             'id' => (string) $task->id,
                             'attributes' => [
                                 'title' => $task->title,
                                 'completed' => (bool) $task->completed
+                            ],
+                            'links' => [
+                                'self' => url() . "/api/tasks/{$task->id}"
                             ]
                         ]
-                    ]);
+            ]);
     }
+
+    /** @test */
+    public function a_user_can_view_all_tasks_in_json_api_format()
+    {
+        $tasks = factory(Task::class, 2)->create();
+
+        $this->json('GET', "/api/tasks")
+            ->seeJsonEquals([
+                        'links' => [
+                            'self' => url('/api/tasks')
+                        ],
+                        'data' => [
+                            [
+                                'type' => 'tasks',
+                                'id' => (string) $tasks[0]->id,
+                                'attributes' => [
+                                    'title' => $tasks[0]->title,
+                                    'completed' => (bool) $tasks[0]->completed,
+                                ],
+                                'links' => [
+                                    'self' => url() . "/api/tasks/{$tasks[0]->id}"
+                                ]
+                            ],
+                            [
+                                'type' => 'tasks',
+                                'id' => (string) $tasks[1]->id,
+                                'attributes' => [
+                                    'title' => $tasks[1]->title,
+                                    'completed' => (bool) $tasks[1]->completed,
+                                ],
+                                'links' => [
+                                    'self' => url() . "/api/tasks/{$tasks[1]->id}"
+                                ]
+                            ]
+
+                        ]
+            ]);
+    }
+
 }
