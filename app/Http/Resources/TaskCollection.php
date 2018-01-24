5ecff084514e05410ef2163a52cfafc14dd43c53
Ali <ma.essa.pk@gmail.com>
2018-01-24 22:00:08 +0000

    Create TaskCollection class to transform a collection of tasks into JSON API format

---
+++ app/Http/Resources/TaskCollection.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Resources;
+
+use App\Task;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TaskCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'links' => [
+                'self' => url() . '/api/tasks'
+            ],
+            'data' => $this->collection
+        ];
+    }
+}
