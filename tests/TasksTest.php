00a62961f806e3f13fc31aae3c8dcc365a8391af
Ali <ma.essa.pk@gmail.com>
2018-01-25 09:26:53 +0000

    Add a new test to TasksTest to test data validation. Also add a new assertion in the test for adding a new task.

--- tests/TasksTest.php
+++ tests/TasksTest.php
@@ -76,7 +76,6 @@ class TasksTest extends TestCase
         $this->json('POST', "/api/tasks", [
                         'data' => [
                             'type' => 'tasks',
-                            'id' => (string) $task->id,
                             'attributes' => [
                                 'title' => $task->title,
                                 'completed' => (bool) $task->completed
@@ -84,9 +83,45 @@ class TasksTest extends TestCase
                         ]
             ]);
 
+        $this->seeJsonEquals([
+                    'data' => [
+                        'type' => 'tasks',
+                        'id' => "1",
+                        'attributes' => [
+                            'title' => $task->title,
+                            'completed' => (bool) $task->completed
+                        ],
+                        'links' => [
+                            'self' => url("/api/tasks/1")
+                        ]
+                    ]
+            ]);
+
         $this->seeInDatabase('tasks', [
                 'title' => $task->title
             ]);
     }
 
+    /** @test */
+    public function when_adding_a_new_task_title_and_completed_values_are_required()
+    {
+        $task = factory(Task::class)->make();
+
+        $response = $this->call('POST', "/api/tasks", [
+                        'data' => [
+                            'type' => 'tasks',
+                            'attributes' => [
+                                'title' => '',
+                                'completed' => ''
+                            ],
+                        ]
+            ]);
+
+        $this->assertEquals(422, $response->status());
+
+        $this->seeJson([
+                            'status' => 422,
+                            'title' => 'The given data was invalid.'
+            ]);
+    }
 }
